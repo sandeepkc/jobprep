9211aff928ddab6eb623485f4c2476dad08d2b21
Sandeep Chaudhary <sandeepc@vmware.com>
2020-01-02 20:40:08 -0800

    Created a dir '2020' in 'java'

---
+++ java/2020/Student.java
@@ -0,0 +1,14 @@
+public class Student{
+    String firstName;
+    String lastName;
+    int age;
+    int grade;
+
+    void Student(String fname, String lname, int age, int grade) {
+        this.firstName = fname;
+        this.lastName = lname;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    
